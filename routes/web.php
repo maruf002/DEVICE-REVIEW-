2cd156a97caad7e052f7c1bace7f6eb3eafc9b87
MARUF ISLAM <mazharulmaruf002@gmail.com>
2020-09-08 16:38:42 +0600

    category-add,missing steps of product category

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//  Route::get('/', function () {
+//       return view('welcome');
+//  });
+ Route::get('/','IndexController@index');
+
+
+    
+
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth','admin']],function(){
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+    Route::resource('product', 'ProductController');
+    Route::resource('category', 'CategoryController');
+
+});
+
+
+
